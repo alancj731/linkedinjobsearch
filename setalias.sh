4dc1a5fbaa295868fcae88c8c9baada301ba9574
alancj731 <alanchenjian@gmail.com>
2025-05-23 09:38:06 -0500

    added a alias to run this application

---
+++ setalias.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd_value=$(pwd)
+echo "alias lkinsearch='node $pwd_value/dist/src/index.js'" >> ~/.bashrc
+echo "Alias 'lkinsearch' added to ~/.bashrc"
+echo "Please run 'source ~/.bashrc' or restart your terminal to use it."

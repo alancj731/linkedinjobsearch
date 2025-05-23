6dfe36f1f1e94ee66eadaf682ca2b55d6c547879
alancj731 <alanchenjian@gmail.com>
2025-05-23 09:50:39 -0500

    fix script execution issue

--- setalias.sh
+++ setalias.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pwd_value=$(pwd)
-echo "alias lkinsearch='node $pwd_value/dist/src/index.js'" >> ~/.bashrc
+echo "alias lkinsearch='cd $pwd_value && node $pwd_value/dist/src/index.js'" >> ~/.bashrc
 echo "Alias 'lkinsearch' added to ~/.bashrc"
 echo "Please run 'source ~/.bashrc' or restart your terminal to use it."
